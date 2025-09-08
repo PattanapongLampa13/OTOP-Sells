7235671bb74cf68097197c8b7642181f804d5860
Nattapon6712732105 <nattaponboonnara@gmail.com>
2025-09-08 10:48:58 +0700

    Implement views and templates for home, product listing, login, and registration; update URL routing and base template

--- webpage/views.py
+++ webpage/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import logout as auth_logout
+from django.shortcuts import redirect
 
-def base(request):
-    return render(request, 'base.html')
-
-def home(request):
+# หน้าหลัก
+def home_view(request):
     return render(request, 'home.html')
 
-def sels(request):
+# หน้ารายการสินค้าทั้งหมด
+def sels_view(request):
     return render(request, 'sels.html')
 
-def map(request):
+# หน้าแผนที่
+def map_view(request):
     return render(request, 'map.html')
 
+# หน้าเข้าสู่ระบบ
+def login_view(request):
+    return render(request, 'login/login.html')
 
-# Create your views here.
+# หน้าสมัครสมาชิก
+def register_view(request):
+    return render(request, 'login/register.html')
