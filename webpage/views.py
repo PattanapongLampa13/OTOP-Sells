bb2c98ffcd7460c73387744d409a1ce29d3d8b29
Nattapon6712732105 <nattaponboonnara@gmail.com>
2025-09-08 10:29:10 +0700

    Add base template and implement home, map, sels views with corresponding HTML templates

--- webpage/views.py
+++ webpage/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+def base(request):
+    return render(request, 'base.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def sels(request):
+    return render(request, 'sels.html')
+
+def map(request):
+    return render(request, 'map.html')
+
+
 # Create your views here.
