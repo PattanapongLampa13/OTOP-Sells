c75209b936bd34d3ca39083637e17e87b0fc9e43
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-09-08 11:11:01 +0700

    Fix View

--- webpage/views.py
+++ webpage/views.py
@@ -5,6 +5,12 @@ from django.shortcuts import render
 from django.contrib.auth import logout as auth_logout
 from django.shortcuts import redirect
 
+from .models import OtopProduct
+import json
+from django.core.serializers import serialize
+
+# Create your views here.
+
 # หน้าหลัก
 def home_view(request):
     return render(request, 'home.html')
@@ -24,3 +30,24 @@ def login_view(request):
 # หน้าสมัครสมาชิก
 def register_view(request):
     return render(request, 'login/register.html')
+
+
+
+def map_view(request):
+    # ดึงข้อมูลสินค้า OTOP ทั้งหมด
+    products = OtopProduct.objects.all()
+    
+    # แปลงข้อมูล QuerySet เป็น GeoJSON format ที่ใช้ง่ายใน JavaScript
+    # หรือจะแปลงเป็น JSON ธรรมดาก็ได้
+    products_json = serialize('json', products, fields=('name', 'latitude', 'longitude'))
+
+    # ดึง API Key จาก settings.py (วิธีที่ปลอดภัยกว่า)
+    # อย่าลืมไปเพิ่ม GOOGLE_MAPS_API_KEY = "your_key" ในไฟล์ settings.py
+    from django.conf import settings
+    api_key = settings.GOOGLE_MAPS_API_KEY
+
+    context = {
+        'products_json': products_json,
+        'api_key': api_key,
+    }
+    return render(request, 'webpage/map_template.html', context)
