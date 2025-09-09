54eab31d4d86dc611e2d258ee8cb3806823e98da
Pongphisut Buahom <zcirpongphisut2549@gmail.com>
2025-09-09 16:09:56 +0700

    build_filem settgins, login fix

--- OTOPsels/settings.py
+++ OTOPsels/settings.py
@@ -140,7 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles_build"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
